36e77218a1bbb27a13742080a6d26776b3ad0e7d
İpek <zekiyeipek_ozmen@hotmail.com>
2024-06-13 21:10:27 +0300

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Java and Python these days, and I'm passionate about creating innovative solutions. 
+    I've developed projects like Money Runnin', which won the Online Game Jam and was selected for the Regional Game Jam. 
+    Check out my <a href="https://github.com/zekiyeipek" target="_blank">Github</a> for more details!`,
+  "se-degree": `This is my Software Engineering degree from Çankaya University. 
+    I'm currently pursuing it with a focus on Algorithm and Data Structures, Software Development Lifecycle, and Web Development.`,
+  "sofa-table": `That's my sofa. I enjoy relaxing here and occasionally developing software applications. 
+    I also actively contribute to projects like WpfRenameApp and CampusCollab. Explore more on my <a href="https://github.com/zekiyeipek" target="_blank">GitHub</a>. 
+    Recently, I volunteered with Solidarités Jeunesses in Faucogney-et-la-mer, France, where I gained valuable teamwork and leadership skills.`,
+  tv: `That's my TV. Lately, I've been immersing myself in game development streams and tutorials.`,
+  bed: `This is where I unwind and brainstorm new software features, like those I developed during my internships at Proven Technologies and SBI Bilişim A.ş. 
+    Connect with me on <a href="https://linkedin.com/in/zekiye-ipek-özmen-370346230" target="_blank">LinkedIn</a> to learn more about my work!`,
+  resume: `Here's my resume, showcasing my skills in Game development, Git, Project Management, and more. 
+    Feel free to <a href="https://drive.google.com/file/d/167yaOPP-AfkNNwyOT31RDLATbcDh_FdO/view?usp=sharing" target="_blank">check it out</a> 
+    and contact me at zekiyeipek_ozmen@hotmail.com for career opportunities!`,
+  projects: `Info about my projects: I've contributed to various software applications, including CampusCollab, aimed at enhancing collaboration among university students and companies. 
+    Explore these projects further on my <a href="https://github.com/zekiyeipek" target="_blank">GitHub</a>.`,
+  library: `My shelves are filled with technical books, reflecting my continuous learning in software development. 
+    I'm fluent in English, Turkish, and I have a driving license. Want to discuss programming or connect? Just drop me a message!`,
+  exit: `To exit Zekiye İpeks Özmen's portfolio, simply close the tab.`,
+};
